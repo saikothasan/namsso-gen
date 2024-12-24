02c2d37c345a5c1067892c2082c50df0a5f57f63
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-24 17:34:37 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import CCGenerator from '@/components/CCGenerator'
-import MassBINGenerator from '@/components/MassBINGenerator'
 import Features from '@/components/Features'
 import About from '@/components/About'
 import FAQ from '@/components/FAQ'
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
       <CCGenerator />
-      <MassBINGenerator />
       <About />
       <Features />
       <FAQ />
